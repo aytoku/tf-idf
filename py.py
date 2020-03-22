c2ea4a21b8e45d831f663ccdc680091c107ad905
Gio <seturidee@mail.ru>
2020-03-22 14:58:48 +0300

    lala

--- py.py
+++ py.py
@@ -1,32 +1,23 @@
 import math
 
-corpus = """
-XAML (eXtensible Application Markup Language) - язык разметки, используемый для инициализации объектов в технологиях на платформе .NET. Применительно к WPF (а также к Silverlight) данный язык используется прежде всего для создания пользовательского интерфейса декларативным путем. Хотя функциональность XAML только графическими интерфейсами не ограничивается: данный язык также используется в технологиях WCF и WF, где он никак не связан с графическим интерфейсом. То есть его область шире. Применительно к WPF мы будем говорить о нем чаще всего именно как о языке разметки, который позволяет создавать декларативным путем интерфейс, наподобие HTML в веб-программировании. Однако опять же повторюсь, сводить XAML к одному интерфейсу было бы неправильно, и далее на примерах мы это увидим.
-XAML - не является обязательной частью приложения, мы вобще можем обходиться без него, создавая все элементы в файле связанного с ним кода на языке C#. Однако использование XAML все-таки несет некоторые преимущества:
-Возможность отделить графический интерфейс от логики приложения, благодаря чему над разными частями приложения могут относительно автономно работать разные специалисты: над интерфейсом - дизайнеры, над кодом логики - программисты.
-Компактность, понятность, код на XAML относительно легко поддерживать.
-При компиляции приложения в Visual Studio код в xaml-файлах также компилируется в бинарное представление кода xaml, которое называется BAML (Binary Application Markup Language). И затем код baml встраивается в финальную сборку приложения - exe или dll-файл.
+arr = """
+Статическое свойство TextProperty является свойством зависимостей, представляя объект System.Windows.DependencyProperty.
+По соглашениям по именованию все свойства зависимостей представляют статические публичные поля (public static) с суффиксом Property.
 """.split("\n")[1:-1]
 
-l_A = corpus[0].lower().split()
-l_B = corpus[1].lower().split()
-l_C = corpus[2].lower().split()
+A = arr[0].lower().split()
+B = arr[1].lower().split()
 
-# считаем bag of words
-word_set = set(l_A).union(set(l_B)).union(set(l_C))
+word_set = set(A).union(set(B))
 
 word_dict_A = dict.fromkeys(word_set, 0)
 word_dict_B = dict.fromkeys(word_set, 0)
-word_dict_C = dict.fromkeys(word_set, 0)
 
-for word in l_A:
+for word in A:
     word_dict_A[word] += 1
 
-for word in l_B:
+for word in B:
     word_dict_B[word] += 1
-
-for word in l_C:
-    word_dict_C[word] += 1
 
 
 def compute_tf(word_dict, l):
@@ -37,9 +28,8 @@ def compute_tf(word_dict, l):
     return tf
 
 
-tf_A = compute_tf(word_dict_A, l_A)
-tf_B = compute_tf(word_dict_B, l_B)
-tf_C = compute_tf(word_dict_C, l_C)
+tf_A = compute_tf(word_dict_A, A)
+tf_B = compute_tf(word_dict_B, B)
 
 
 def compute_idf(strings_list):
@@ -55,7 +45,7 @@ def compute_idf(strings_list):
     return idf
 
 
-idf = compute_idf([word_dict_A, word_dict_B, word_dict_C])
+idf = compute_idf([word_dict_A, word_dict_B])
 
 
 def compute_tf_idf(tf, idf):
@@ -67,5 +57,5 @@ def compute_tf_idf(tf, idf):
 
 tf_idf_A = compute_tf_idf(tf_A, idf)
 tf_idf_B = compute_tf_idf(tf_B, idf)
-tf_idf_C = compute_tf_idf(tf_C, idf)
 print(tf_idf_A)
+print(tf_idf_B)
